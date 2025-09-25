2461670611b9ddcd9d07c360ef5344bc738f830a
casey.Q.Wood <caseywood252@gmail.com>
2025-09-25 16:22:25 -0600

    React-Router with animations first draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { Routes, Route, useNavigate, useLocation } from 'react-router';
+import { AnimatePresence } from 'motion/react';
 import './App.css'
 import LoginScreen from './components/Login'
 import ResultsBarChart from './components/resultsBarChart';
 import UnitForm from './components/unitForm/UnitForm';
 import Header from './components/header';
+import { motion } from "motion/react"
+
+
 type WoundTallies = Record<number, number>;
 
 
@@ -11,26 +16,66 @@ function App() {
   const [simData, setSimData] = useState<WoundTallies>()
   // const [openAside, setOpenAside] = useState<boolean>(false)
   const [loggedIn, setLoggedIn] = useState(false)
+  const navigate = useNavigate();
+  const location = useLocation()
+
+
+  const page = {
+    initial: { opacity: 0, y: 16 },
+    animate: { opacity: 1, y: 0, transition: { duration: 0.35 } },
+    // exit: { opacity: 0, y: -16, transition: { duration: 0.25 } },
+  };
 
   return (
-    <div id='main'>
-      <LoginScreen loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-      {loggedIn ?
-        <article>
-          <UnitForm setSimData={setSimData} />
-          {simData ?
-            <ResultsBarChart results={simData} />
-            : null
-          }
-        </article> : null}
-      {/* <Header openAside={openAside} setOpenAside={setOpenAside} />
+    <div key="test" id='main'>
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.main
+          key={location.pathname}            // or: location.key
+          variants={page}
+          initial="initial"
+          animate="animate"
+        // exit="exit"
+        >
+          <Routes location={location}>
+            <Route path="/home" element={
+              <motion.article
+                // initial={{ opacity: 0, x: 100 }}
+                // animate={{ opacity: 1, x: 0 }}
+                // exit={{ opacity: 0, x: -100 }}
+                // transition={{ duration: 0.5 }}
+                key="home"
+              >
+                <UnitForm setSimData={setSimData} />
+                {simData ?
+                  <ResultsBarChart results={simData} />
+                  : null
+                }
+              </motion.article>
+            } />
+            <Route path="/" element={<LoginScreen key="login-screen" loggedIn={loggedIn} setLoggedIn={() => navigate("/home")} />} />
+          </Routes>
+
+          {/* {loggedIn ?
+          // <article>
+          //   <UnitForm setSimData={setSimData} />
+          //   {simData ?
+          //     <ResultsBarChart results={simData} />
+          //     : null
+          //   }
+          // </article>
+          null :
+          
+        } */}
+          {/* <Header openAside={openAside} setOpenAside={setOpenAside} />
       <div id="main">
          {openAside ? <aside>aside</aside> : null}
 
 
       </div>
       <footer>Footer</footer> */}
-    </div>
+        </motion.main>
+      </AnimatePresence>
+    </div >
   )
 }
 
