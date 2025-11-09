4826e57df9d36465a2978e9cfad257323cd84b7d
casey.Q.Wood <caseywood252@gmail.com>
2025-11-08 21:35:00 -0600

    sentry config and install

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "na-osq",
+    project: "javascript-react"
+  })],
+
   server: {
     host: "0.0.0.0",
     port: 5173,
     strictPort: true,
   },
+
+  build: {
+    sourcemap: true
+  }
 });
